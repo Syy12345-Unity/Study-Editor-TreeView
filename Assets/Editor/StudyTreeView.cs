1f5c7a71700c4455fedc4926cc50224d8a12acc6
Syy <shirakawa.yy@gmail.com>
2018-08-13 23:33:40 +0900

    Add custom treeView

---
+++ Assets/Editor/StudyTreeView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.IMGUI.Controls;
+
+public class StudyTreeView : TreeView {
+    public StudyTreeView(TreeViewState state) : base(state)
+    {
+
+    }
+
+    protected override TreeViewItem BuildRoot()
+    {
+        throw new System.NotImplementedException();
+    }
+}
